cb02d2bb4fd75b4fa552ca0d15f08475f756ecd1
Siraj Kimuli <pworld.designer@gmail.com>
2022-02-10 22:03:01 +0300

    support laravel 9

---
+++ src/Exceptions/CouldNotSendNotification.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace NotificationChannels\SmsSpeedaMobile\Exceptions;
+
+class CouldNotSendNotification extends \Exception
+{
+    public static function serviceRespondedWithAnError($response)
+    {
+        return new static("Descriptive error message.");
+    }
+}
---
+++ src/SmsSpeedaMobileMessage.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace NotificationChannels\SmsSpeedaMobile;
+
+use Illuminate\Support\Arr;
+
+class SmsSpeedaMobileMessage
+{
+    // Message structure here
+}
